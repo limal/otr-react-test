5423f24cec45ffd930aaaf873185c6ea1159b92b
Lukasz Wolnik <lukasz@wolnik.co.uk>
2019-05-21 20:46:04 +0100

    Build added

---
+++ precache-manifest.ef3873cbbded3911285496486cd5044e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ac07c69e1bef8e8f6c063b0ef8b608",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7e9c922a3e195d5bab1",
+    "url": "/static/css/main.efd156d6.chunk.css"
+  },
+  {
+    "revision": "bc001c008d0bc26b970d",
+    "url": "/static/js/2.d2ee93fa.chunk.js"
+  },
+  {
+    "revision": "d7e9c922a3e195d5bab1",
+    "url": "/static/js/main.aad81da2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
