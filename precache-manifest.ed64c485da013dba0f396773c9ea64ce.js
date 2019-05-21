10074106a290f75b5ba24b224dd25dc781f6cb46
Lukasz Wolnik <lukasz@wolnik.co.uk>
2019-05-21 20:56:24 +0100

    Updated homepage for GitHub Pages

---
+++ precache-manifest.ed64c485da013dba0f396773c9ea64ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071069fe78c7e144cc0d4b2bfa9b99e0",
+    "url": "/otr-react-test/index.html"
+  },
+  {
+    "revision": "d7e9c922a3e195d5bab1",
+    "url": "/otr-react-test/static/css/main.efd156d6.chunk.css"
+  },
+  {
+    "revision": "bc001c008d0bc26b970d",
+    "url": "/otr-react-test/static/js/2.d2ee93fa.chunk.js"
+  },
+  {
+    "revision": "d7e9c922a3e195d5bab1",
+    "url": "/otr-react-test/static/js/main.aad81da2.chunk.js"
+  },
+  {
+    "revision": "2f5a2b8ce4ac0eb88a1a",
+    "url": "/otr-react-test/static/js/runtime~main.01447df6.js"
+  }
+]);
